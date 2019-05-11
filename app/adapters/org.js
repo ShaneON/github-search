500cd62d8274c0fdb51d64c3f133362386470e69
Shane <sgoneill89@gmail.com>
2019-05-11 22:20:04 +0100

    removed my auth token

--- app/adapters/org.js
+++ app/adapters/org.js
@@ -2,8 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   headers: {
-    'Accept': 'application/vnd.github.v3+json',
-    'Authorization': 'token 85c12bb9175efb454e43467a1829fecf40789822'
+    'Accept': 'application/vnd.github.v3+json'
   },
 
   urlForFindRecord(id, modelName) {
